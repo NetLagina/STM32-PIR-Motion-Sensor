7db2b74388a5d1af32f7562af4d43eca4d0a9330
Zhan Poplavskyi <gan.popl@gmail.com>
2021-05-06 12:45:23 +0300

    Add license notes to some files

--- Core/Inc/led.h
+++ Core/Inc/led.h
@@ -1,8 +1,19 @@
-/*
- * led.h
+/**
+ * STM32 PIR Motion Sensor - example of use PIR motion sensor with stm32.
+ * Copyright (C) 2021  Zhan Poplavskyi
  *
- *  Created on: Jul 16, 2019
- *      Author: NetL
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef LED_H_
